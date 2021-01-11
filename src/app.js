c601acfae545e8d8ff30e8012d013da59615d395
m6ttw <matthewrhyswilliams@live.com>
2021-01-11 18:44:36 +0000

    finished mvp

--- src/app.js
+++ src/app.js
@@ -3,5 +3,17 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
+    data: {
+      todos: [
+        "Buy shopping", "Clean bathroom", "Car's MOT"
+      ],
+      newTask: ""
+    },
+    methods: {
+      saveNewTask: function(){
+        this.todos.push(this.newTask);
+        this.newTask = "";
+      }
+    }
   });
 });
